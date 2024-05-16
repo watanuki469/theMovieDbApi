d25b7c804ed3d1f3a4aca4ca0741a5f923a14ebf
Vasiliev <75292914+watanuki469@users.noreply.github.com>
2024-05-16 09:26:16 +0700

    fix vercel

--- dist/index.js
+++ dist/index.js
@@ -6,16 +6,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const authController_1 = require("./models/authController");
 const body_parser_1 = __importDefault(require("body-parser"));
+const http_1 = __importDefault(require("http"));
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = 5000;
 app.use(cors());
 app.use(body_parser_1.default.json());
-mongoose_1.default.connect('mongodb+srv://admin:vNyLVQug8B7quWE4@cluster0.3avkh2c.mongodb.net/theMovieDb');
 app.post('/login', authController_1.login);
 app.get('/users', authController_1.getAllUsers);
 app.post('/register', authController_1.createUser);
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+const server = http_1.default.createServer(app);
+mongoose_1.default.connect('mongodb+srv://admin:vNyLVQug8B7quWE4@cluster0.3avkh2c.mongodb.net/theMovieDb').then(() => {
+    console.log('MongoDb Connected');
+    server.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((err) => {
+    console.log({ err });
+    process.exit(1);
 });
--- index.ts
+++ index.ts
@@ -3,19 +3,28 @@ import mongoose from 'mongoose';
 import Notes2 from './models/Notes2';
 import { createUser, getAllUsers, login } from './models/authController';
 import bodyParser from 'body-parser';
+import http from "http";
 
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT=5000
-app.use(cors()); 
+const PORT = 5000;
+
+app.use(cors());
 app.use(bodyParser.json());
-mongoose.connect('mongodb+srv://admin:vNyLVQug8B7quWE4@cluster0.3avkh2c.mongodb.net/theMovieDb');
 
 app.post('/login', login);
 app.get('/users', getAllUsers);
 app.post('/register', createUser);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+const server = http.createServer(app);
+
+mongoose.connect('mongodb+srv://admin:vNyLVQug8B7quWE4@cluster0.3avkh2c.mongodb.net/theMovieDb').then(() => {
+    console.log('MongoDb Connected');
+    server.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((err) => {
+    console.log({ err });
+    process.exit(1);
 });
